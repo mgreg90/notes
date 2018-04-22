eaf4407f95309fd84832984fce776ef41ebd3d29
mgregory-carecloud <mgregory@carecloud.com>
2018-04-21 22:51:08 -0400

    Built basic notes command

--- notes.rb
+++ notes.rb
@@ -4,7 +4,11 @@ require 'bundler/inline'
 gemfile do
   source 'https://rubygems.org'
   gem 'playwright-cli', require: 'playwright/cli'
+  gem 'git', '~> 1.3'
+  gem 'pry-nav'
 end
+
+require 'fileutils'
 
 module Notes
   module CLI
@@ -12,16 +16,58 @@ module Notes
       extend Playwright::CLI::Registry
 
       class Root < Playwright::CLI::Command
-        desc "Says a greeting to the name given. This is an example."
+        desc "Opens your notes file."
 
-        argument :name, required: true, desc: 'Whom shall I greet?'
+        NOTES_FILE_NAME_ROOT = '.notes'
+        GITIGNORE_FILE_NAME = '.gitignore'
+        GITIGNORE_LINE = '.notes*'
 
-        example [
-          "\"Johnny Boy\" #=> Why, hello Johnny Boy!"
-        ]
+        def call(**)
+          create_notes_file unless notes_file.file?
+          add_to_gitignore
+          open_notes_file
+        end
 
-        def call(name:, **)
-          display.print "Why, hello #{name}!"
+        def git
+          @git ||= Git.open(Dir.pwd) if Dir.exists?(File.join(Dir.pwd, '.git'))
+        end
+
+        def git?
+          !!git
+        end
+
+        def notes_file
+          @notes_file ||= Pathname.new(File.join(Dir.pwd, notes_filename))
+        end
+
+        def notes_filename
+          "#{NOTES_FILE_NAME_ROOT}-#{git.branch if git?}"
+        end
+
+        def gitignore_file
+          @gitignore_file ||= Pathname.new(File.join(Dir.pwd, GITIGNORE_FILE_NAME))
+        end
+
+        def create_notes_file
+          FileUtils.touch(notes_file)
+          notes_file.write("#{git.branch.name}\n\n")
+        end
+
+        def open_notes_file
+          os.open_editor path: notes_file
+        end
+
+        def add_to_gitignore
+          if add_to_gitignore?
+            gitignore_file.open('a') do |f|
+              f << "\n#{GITIGNORE_LINE}\n"
+            end
+          end
+        end
+
+        def add_to_gitignore?
+          gitignore_file.file? &&
+            !gitignore_file.readlines.map(&:chomp).include?(GITIGNORE_LINE)
         end
 
       end
